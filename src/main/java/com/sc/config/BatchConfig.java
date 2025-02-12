5740ddc6c965a6b43b0a9b4782c9431014e9b6e2
hunjin-choi <71176033+hunjin-choi@users.noreply.github.com>
2025-02-12 22:10:35 +0900

    JobLauncherApllicationRunner 생성하는 방식 -> JobLauncher 이용하여 직접 Job을 실행하는 방식

--- src/main/java/com/sc/config/BatchConfig.java
+++ src/main/java/com/sc/config/BatchConfig.java
@@ -1,11 +1,13 @@
 package com.sc.config;
 
-import org.springframework.batch.core.Job;
-import org.springframework.batch.core.Step;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.support.DefaultBatchConfiguration;
 import org.springframework.batch.core.explore.JobExplorer;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
 import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.ResourcelessJobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
 import org.springframework.boot.autoconfigure.batch.JobLauncherApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -16,14 +18,26 @@ import com.sc.batch.CustomProcessor;
 import com.sc.batch.CustomReader;
 import com.sc.batch.CustomWriter;
 
-@Configuration
-public class BatchConfig {
+import java.time.LocalDateTime;
+import java.util.UUID;
 
+@Configuration
+@EnableBatchProcessing
+public class BatchConfig extends DefaultBatchConfiguration {
 	@Bean
-	public JobLauncherApplicationRunner jobLauncherApplicationRunner(JobLauncher jobLauncher, JobExplorer jobExplorer, JobRepository jobRepository) {
-		return new JobLauncherApplicationRunner(jobLauncher, jobExplorer, jobRepository);
+	public JobParameters jobParameters() {
+		JobParameters jobParameters = new JobParametersBuilder()
+				.addString("prodDt", LocalDateTime.now().toString())
+				.addString("uuid", UUID.randomUUID().toString()) // resourcelsssJobRepository 사용할거면 해당 파라미터는 불필요
+				.toJobParameters();
+
+		return jobParameters;
 	}
 
+	@Bean
+	public JobRepository jobRepository() {
+		return new ResourcelessJobRepository();
+	}
 	@Bean
 	Job createJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
 		return new JobBuilder("job", jobRepository)
